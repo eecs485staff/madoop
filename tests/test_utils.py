f3ce36f098202ada2676aafa064a398232c5db25
Jared Zhang <jaredzh@umich.edu>
2021-10-13 13:44:08 -0400

    Utility/helper function tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 from madoop.__main__ import check_num_keys
 from madoop.__main__ import check_shebang
 from madoop.__main__ import part_filename
+import pathlib
 
 def test_part_filename():
     part_num = 3
@@ -8,3 +9,6 @@ def test_part_filename():
     assert(filename == ('part-'+('%05d'%part_num)))
 
 
+def test_check_shebang():
+    file = pathlib.Path("tests/testdata/word_count/map.py")
+    check_shebang(file)
