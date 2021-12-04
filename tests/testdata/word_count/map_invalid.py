7d43e64d396710be122ad04358516ad9ba7ecbad
Andrew DeOrio <awdeorio@umich.edu>
2021-12-04 08:35:33 -0500

    Simplify intentionally bad exes

--- tests/testdata/word_count/map_invalid.py
+++ tests/testdata/word_count/map_invalid.py
@@ -3,9 +3,4 @@
 
 import sys
 
-for line in sys.stdin:
-    words = line.split()
-    for word in words:
-        print(word + "\t1")
-
 sys.exit(1)
--- tests/testdata/word_count/reduce_invalid.py
+++ tests/testdata/word_count/reduce_invalid.py
@@ -1,16 +1,3 @@
 """Invalid reduce executable Reduce example is missing a shebang."""
 
-import sys
-import collections
-
-WORDDICT = {}
-for line in sys.stdin:
-    word = line.split("\t")[0]
-    if word in WORDDICT:
-        WORDDICT[word] += 1
-    else:
-        WORDDICT[word] = 1
-
-SORTEDDICT = collections.OrderedDict(sorted(WORDDICT.items()))
-for key in SORTEDDICT:
-    print(key, SORTEDDICT[key])
+pass
