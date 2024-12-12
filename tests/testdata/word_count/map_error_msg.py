ea9a7fd41e9fe3f7d4fca91861a08960052d604d
Andrew DeOrio <awdeorio@umich.edu>
2024-12-12 15:18:37 -0500

    Fix tests failing on is_executable() check.  Should fail later.

--- tests/testdata/word_count/map_error_msg.py
+++ tests/testdata/word_count/map_error_msg.py
@@ -3,6 +3,10 @@
 
 import sys
 
-sys.stdout.write("Map error message to stdout\n")
-sys.stderr.write("Map error message to stderr\n")
-sys.exit(1)
+
+# Avoid error on executable check which has an empty string input
+input_lines_n = sum(1 for _ in sys.stdin)
+if input_lines_n > 1:
+    sys.stdout.write("Map error message to stdout\n")
+    sys.stderr.write("Map error message to stderr\n")
+    sys.exit(1)
--- tests/testdata/word_count/partition_error_msg.py
+++ tests/testdata/word_count/partition_error_msg.py
@@ -3,6 +3,9 @@
 
 import sys
 
-sys.stderr.write("Partition error message to stdout\n")
-sys.stderr.write("Partition error message to stderr\n")
-sys.exit(1)
+# Avoid error on executable check which has an empty string input
+input_lines_n = sum(1 for _ in sys.stdin)
+if input_lines_n > 1:
+    sys.stderr.write("Partition error message to stdout\n")
+    sys.stderr.write("Partition error message to stderr\n")
+    sys.exit(1)
